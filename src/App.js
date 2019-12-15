c90ce3500f31daf7071c91cd99097e484e951d95
Kristian Rosland <kristian.rosland@gmail.com>
2019-12-15 16:03:34 +0100

    Initial layout with placeholders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from "react";
+import logo from "./images/logo-hvit-nopadding.png";
+import nvbf from "./images/logo-hvit-nopadding.png";
 import "./App.css";
 
 function App() {
-  return <div className="app">The experience</div>;
+  return (
+    <div className="app">
+      <div className="header-container">
+        <div className="info-container">
+          <h1 className="title"> The Experience </h1>
+          <h2 className="subtitle"> Oslo// 1.-3. May 2020 </h2>
+          <p className="description">
+            For up and coming vollyeball teams in Europe
+          </p>
+        </div>
+
+        <div className="button-container">
+          <button className="apply-button"> Apply now </button>
+        </div>
+        <img src={logo} className="x-logo" />
+      </div>
+      <div className="bottom-content">
+        <h2 className="the-experience-quote">The Experience</h2>
+        <p className="the-experience-sound">/ðə ɛkˈspɪərɪəns/</p>
+        <p className="the-experience-meaning">
+          the event or occurence which leads an impression on someone
+        </p>
+        <img alt="nvbf logo" src={nvbf} className="nvbf-logo" />
+        <p className="nvbf-title">Supported by HVBF</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./fonts/AvenirLTStd-Book.otf";
+import "./fonts/AvenirLTStd-Medium.otf";
+import "./fonts/AvenirLTStd-Roman.otf";
+import "./fonts/Avenir-Black.otf";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
