6a2efc1f6b2573be0aa0a89306b242c1a0669b68
Kristian Rosland <kristian.rosland@gmail.com>
2020-01-15 08:37:35 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.456aedd2730968284322ea15e5dc08d0.js"
+  "/precache-manifest.b08cbf1777124a2397522bc55615bffb.js"
 );
 
 self.addEventListener('message', (event) => {
