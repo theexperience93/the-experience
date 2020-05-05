07fd9496d05084386a64d2ef5c1fcde100f89b99
Kristian Rosland <kristian.rosland@gmail.com>
2020-05-05 15:33:42 +0200

    Covid feature toggle

--- src/App.js
+++ src/App.js
@@ -11,45 +11,58 @@ const App = () => {
     ReactGA.initialize("UA-154697902-1");
     ReactGA.event({
       category: "View",
-      action: "Page loaded"
+      action: "Page loaded",
     });
   }, []);
 
   const inputRef = useRef(null);
+
+  const featureToggle = false;
 
   return (
     <div className="app">
       <div className="header-container">
         <div className="info-container">
           <h1 className="title"> The Experience </h1>
-          <h2 className="subtitle"> Oslo// 1-3 May 2020 </h2>
-          <p className="description">
-            For up and coming volleyball teams in Europe with a prizepool of
-            €20&nbsp;000
-          </p>
+          {featureToggle && (
+            <>
+              <h2 className="subtitle"> Oslo// 1-3 May 2020 </h2>
+              <p className="description">
+                For up and coming volleyball teams in Europe with a prizepool of
+                €20&nbsp;000
+              </p>
+            </>
+          )}
         </div>
         <div className="hexagon-top-yellow" />
         <div className="hexagon-left-blue" />
         <div className="hexagon-top-blue" />
         <div className="hexagon-right-yellow" />
-        <div className="button-container">
-          <button
-            className="apply-button"
-            onClick={() => {
-              window.scrollTo({
-                behavior: "smooth",
-                top: inputRef.current.offsetTop
-              });
-              inputRef.current.focus();
-            }}
-          >
-            Apply now
-          </button>
-        </div>
+        {featureToggle && (
+          <div className="button-container">
+            <button
+              className="apply-button"
+              onClick={() => {
+                window.scrollTo({
+                  behavior: "smooth",
+                  top: inputRef.current.offsetTop,
+                });
+                inputRef.current.focus();
+              }}
+            >
+              Apply now
+            </button>
+          </div>
+        )}
         <img src={logo} className="x-logo" alt="x-logo" />
       </div>
       <div className="bottom-content">
-        <Form inputRef={inputRef} />
+        <p className="covid-information">
+          The Experience has been postponed due to the Covid-19 situation. We
+          will be back with more information at a later time. Until then, stay
+          safe!
+        </p>
+        {featureToggle && <Form inputRef={inputRef} />}
         <h2 className="the-experience-quote">The Experience</h2>
         <p className="the-experience-sound">/ðə ɛkˈspɪərɪəns/</p>
         <p className="the-experience-meaning">
